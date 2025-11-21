6c63fc7bb04912574cdd6c6988b241bf6907ddd2
Bianca <biancatsouza@gmail.com>
2025-11-21 17:23:25 -0300

    Corrige mensagem de erro do cadastro de produtos

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -8,10 +8,11 @@ public class Produto
 
     [Required(ErrorMessage = "O Preço é obrigatório.")]
     [Range(0.01, (double)decimal.MaxValue, ErrorMessage = "O preço deve ser maior que zero.")]
+    [RegularExpression(@"^\d{1,4},\d{2}$", ErrorMessage = "Insira um valor válido.")]
     public decimal Preco { get; set; }
 
     [Required(ErrorMessage = "O Estoque é obrigatório.")]
-    [Range(0, int.MaxValue, ErrorMessage = "O estoque não pode ser negativo.")]
+    [Range(0, int.MaxValue, ErrorMessage = "O estoque não pode ser negativo.")]    
     public int Estoque { get; set; }
     public string? ImagemUrl { get; set; }
     public int Destaque { get; set; }
